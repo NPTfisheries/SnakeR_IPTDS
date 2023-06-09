adc0cdf0e6f1973011429832369a784f18e89c62
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-06-09 10:54:10 -0600

    initiated leaflet script

---
+++ R/02_sr_iptds_leaflet.R
@@ -0,0 +1,34 @@
+# -----------------------
+# Author: Mike Ackerman & Ryan N. Kinzer
+# Purpose: Create interactive maps for Snake R. IPTDS prioritization and planning
+# 
+# Created: June 9, 2023
+# Last Modified: June 9, 2023
+# Notes: Much of this is based on a previous script iptds_planning.R from RK
+# 
+
+# -----------------------
+# SETUP
+
+# clear environment
+rm(list = ls())
+
+# load packages
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+# library(janitor)
+# library(here)
+
+# -----------------------
+# COMPILE DATA
+
+# get states
+pnw_states = ne_states(country = "united states of america", returnclass = "sf") %>%
+  select(name) %>%
+  filter(name %in% c("Idaho", "Oregon", "Washington"))
+
+# -----------------------
+# BUILD LEAFLET
+
+# use Section 3 in Ryan's iptds_planning script
