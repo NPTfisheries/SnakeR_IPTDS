18adae426a5828e86a96e0fa33fa63141c8c2b7b
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-04-12 10:03:32 -0600

    draft recommendations leaflet

--- R/ipdts_recommendations_leaflet.R
+++ R/ipdts_recommendations_leaflet.R
@@ -28,9 +28,13 @@ iptds_cur = read_excel(here("docs/Snake River IPTDS Prioritization 20240410.xlsx
                        sheet = "SR_IPTDS_Sites") %>%
   st_as_sf(coords = c("longitude", "latitude"),
            crs = 4326)
+
 # iptds recommendations
 iptds_rec = read_excel(here("data/derived_data/proposed_iptds_sites_20240411.xlsx"),
                        sheet = "Sheet1") %>%
+  filter(!site_code %in% c("EFS (Proposed)",
+                           "WB1",
+                           "SLT (Proposed)")) %>%
   st_as_sf(coords = c("longitude", "latitude"),
            crs = 4326)
   
@@ -42,8 +46,30 @@ sthd_pops = sth_pop %>%
 chnk_pops = spsm_pop %>%
   select(TRT_POPID, POP_NAME, MPG) ; rm(spsm_pop)
 
-# traps and weirs
-load(here("data/derived_data/spatial/sr_traps_weirs.rda"))
+# dabom mrr sites
+load("C:/Git/SnakeRiverFishStatus/data/configuration_files/site_config_LGR_20240304.rda") ; rm(flowlines, node_paths, pc_nodes)
+dabom_sites = parent_child %>%
+  unlist() %>%
+  unique() %>%
+  tibble(site_code = .) %>%
+  # remove dams and downriver sites
+  filter(!site_code %in% c("LGR", "GRS", "GOA", "LMA", "IHR", "MCN", "JDA", "TDA", "BON", 
+                           "PRA", "PRO", "WWB", "UMW", "JD1", "DRM", "KLR", "FID", "RCX", "LWL", "WRA")) %>%
+  pull()
+
+mrr_sites = configuration %>%
+  mutate(node = str_remove(node, "_D|_U")) %>%
+  filter(node %in% dabom_sites) %>%
+  filter(nchar(site_code) > 3) %>%
+  select(site_code,
+         node,
+         site_type,
+         site_name,
+         site_description,
+         latitude,
+         longitude) %>%
+  st_as_sf(coords = c("longitude", "latitude"),
+           crs = 4326)
 
 # streams and steelhead major/minor spawning areas
 load(here("data/derived_data/spatial/steelhead_gis_data.rda"))
@@ -60,14 +86,17 @@ chnk_spawn = readRDS(here("data/derived_data/spatial/spsm_spwn_areas.rds")) %>%
 # -----------------------
 # SET SOME COLORS
 sthd_mpg_col = colorFactor(palette = "Dark2", domain = sthd_pops$MPG)
-sthd_spawn_col = colorFactor(palette = c('skyblue','navy'), domain = sthd_spawn$TYPE, reverse = TRUE)
+sthd_spawn_col = colorFactor(palette = c("skyblue", "navy"), domain = sthd_spawn$TYPE, reverse = TRUE)
 chnk_mpg_col = colorFactor(palette = "Set1", domain = chnk_pops$MPG)
-chnk_spawn_col = colorFactor(palette = c('springgreen','darkgreen'), domain = chnk_spawn$TYPE, reverse = TRUE)
-rec_col = colorFactor(palette = c("black", "blue", "green", "red"), domain = iptds_rec$recommendation)
-
-#int_om_col = colorFactor(c("gray", "red"), domain = iptds$integrated_om_site)
-#status_and_trends_col = colorFactor(c('red', 'orange', 'yellow'), domain = iptds$adult_status_trends)
-#funding_col <- colorFactor(palette = 'Set3', domain = iptds$bpa_funding)
+chnk_spawn_col = colorFactor(palette = c("springgreen", "darkgreen"), domain = chnk_spawn$TYPE, reverse = TRUE)
+rec_col = colorFactor(palette = c("black", "red3", "royalblue4", "goldenrod3"), 
+                      domain = iptds_rec$recommendation,
+                      levels = c("Continue Funding", "Proposed New Site", "Candidate for O&M Project", "Decommission, Remove, or Transfer"))
+cur_col = colorFactor(palette = c("gray50", "black"), domain = iptds_cur$integrated_om_site)
+priority_col = colorFactor(c("red", "orange", "yellow"), 
+                           domain = iptds_rec$action_priority, 
+                           levels = c("HIGH", "MED", "LOW"),
+                           na.color = "transparent")
 
 # -----------------------
 # BUILD LEAFLET
@@ -101,7 +130,7 @@ sr_iptds_leaflet = base %>%
               weight = 1,
               color = "black",
               opacity = 1,
-              label = ~paste0(POP_NAME, ": ", MSA_NAME)) %>%
+              label = ~paste0(POP_NAME, ": ", MSA_NAME, ", ", TYPE)) %>%
   # chinook salmon populations
   addPolygons(data = chnk_pops,
               group = "Sp/Sum Chinook Populations",
@@ -125,7 +154,27 @@ sr_iptds_leaflet = base %>%
               weight = 1,
               color = "black",
               opacity = 1,
-              label = ~paste0(POP_NAME, ": ", MSA_NAME)) %>%
+              label = ~paste0(POP_NAME, ": ", MSA_NAME, ", ", TYPE)) %>%
+  # add current iptds
+  addCircles(data = iptds_cur,
+             group = "Current IPTDS Sites",
+             label = ~site_code,
+             color = ~cur_col(integrated_om_site),
+             opacity = 1,
+             weight = 10,
+             popup = paste("<b>Site Code:</b>", iptds_cur$site_code, "</br>",
+                           "<b>Site Name:</b>", iptds_cur$site_name, "</br>",
+                           "<b>Stream:</b>", iptds_cur$stream, "</br>",
+                           "<b>Node Count:</b>", iptds_cur$node_count, "</br>",
+                           "<b>Antenna Count:</b>", iptds_cur$antenna_count, "</br>",
+                           "<b>PTAGIS Active:</b>", iptds_cur$ptagis_active, "</br>",
+                           "<b>Detection Probabilities:</b>", iptds_cur$detection_prob, "</br>",
+                           "<b>Biomark Integrated O&M Site:</b>", iptds_cur$integrated_om_site, "</br>",
+                           "<b>Status and Trends:</b>", iptds_cur$adult_status_trends, "</br>",
+                           "<b>Current Funding:</b>", iptds_cur$current_funding, "</br>",
+                           "<b>BPA Funding Type:</b>", iptds_cur$bpa_funding, "</br>",
+                           "<b>O&M Agency:</b>", iptds_cur$om_agency, "</br>",
+                           "<b>Site Description:</b>", iptds_cur$site_description, "</br>")) %>%
   # add iptds recommendations
   addCircles(data = iptds_rec,
              group = "IPTDS Recommendations",
@@ -134,164 +183,74 @@ sr_iptds_leaflet = base %>%
              opacity = 1,
              weight = 10,
              popup = paste("<b>Site Code:</b>", iptds_rec$site_code, "</br>",
-                           "<b>Recommendation:</b>", iptds_rec$recommendation, "</br>",
                            "<b>Priority:</b>", iptds_rec$action_priority, "</br>",
-                           "<b>Potential Upgrades:</b>", iptds_rec$upgrades, "</br>",
                            "<b>Notes:</b>", iptds_rec$notes, "</br>")) %>%
+  # dabom mrr sites
+  addCircles(data = mrr_sites,
+             group = "DABOM MRR Sites",
+             label = ~site_code,
+             color = "hotpink",
+             opacity = 1,
+             weight = 5,
+             popup = paste("<b>Site Code:</b>", mrr_sites$site_code, "</br>",
+                           "<b>Site Name:</b>", mrr_sites$site_name, "</br>",
+                           "<b>DABOM Node:</b>", mrr_sites$node, "</br>")) %>%
+  # color by status and trends tier
+  addCircleMarkers(data = iptds_rec,
+                   group = "Action Priority",
+                   color = ~priority_col(action_priority)) %>%
   # control layers
   addLayersControl(baseGroups = c("Steelhead Populations",
                                   "Steelhead Spawning Areas",
                                   "Sp/Sum Chinook Populations",
                                   "Sp/Sum Chinook Spawning Areas"),
-                   overlayGroups = c("IPTDS Recommendations"),
-                   options = layersControlOptions(collapsed = FALSE))
-sr_iptds_leaflet
-
-
-sr_iptds_leaflet = base %>%
-  # chinook salmon populations
-  addPolygons(data = chnk_pops,
-              group = "Chinook Salmon Populations",
-              fillColor = ~chnk_mpg_col(MPG),
-              #fillColor = "blue",
-              fillOpacity = 0.2,
-              stroke = T,
-              weight = 2,
-              color = "black",
-              opacity = 1,
-              label = ~paste0(TRT_POPID, ": ", POP_NAME),
-              popup = paste("<b>sp/sum Chinook salmon</b></br>",
-                            "<b>Pop ID:</b>", chnk_pops$TRT_POPID, "</br>",
-                            "<b>Pop Name:</b>", chnk_pops$POP_NAME, "</br>",
-                            "<b>MPG:</b>", chnk_pops$MPG, "</br>")) %>%
-  # steelhead populations
-  addPolygons(data = sthd_pops,
-              group = "Steelhead Populations",
-              fillColor = ~sthd_mpg_col(MPG),
-              #fillColor = "red",
-              fillOpacity = 0.2,
-              stroke = T,
-              weight = 2,
-              color = "black",
-              opacity = 1,
-              label = ~paste0(TRT_POPID, ": ", POP_NAME),
-              popup = paste("<b>Steelhead</b></br>",
-                            "<b>Pop ID:</b>", sthd_pops$TRT_POPID, "</br>",
-                            "<b>Pop Name:</b>", sthd_pops$POP_NAME, "</br>",
-                            "<b>MPG:</b>", sthd_pops$MPG, "</br>")) %>%
-  # chinook major/minor spawning areas
-  addPolygons(data = chnk_spawn,
-              group = "Chinook Spawning Areas",
-              fillColor = ~chnk_spawn_col(TYPE),
-              fillOpacity = 0.2,
-              stroke = T,
-              weight = 1,
-              color = "black",
-              opacity = 1,
-              label = ~paste0(POP_NAME, ": ", MSA_NAME)) %>%
-  # steelhead major/minor spawning areas
-  addPolygons(data = sthd_spawn_wgs84,
-              group = "Steelhead Spawning Areas",
-              fillColor = ~sthd_spawn_col(TYPE),
-              fillOpacity = 0.2,
-              stroke = T,
-              weight = 1,
-              color = "black",
-              opacity = 1,
-              label = ~paste0(POP_NAME, ": ", MSA_NAME)) %>%
-  # add iptds site information
-  addCircles(data = iptds,
-             group = "All IPTDS Sites",
-             label = ~site_code,
-             color = "black",
-             opacity = 1,
-             weight = 10,
-             popup = paste("<b>Site Code:</b>", iptds$site_code, "</br>",
-                           "<b>Site Name:</b>", iptds$site_name, "</br>",
-                           "<b>Stream:</b>", iptds$stream, "</br>",
-                           "<b>Node Count:</b>", iptds$node_count, "</br>",
-                           "<b>Antenna Count:</b>", iptds$antenna_count, "</br>",
-                           "<b>PTAGIS Active:</b>", iptds$ptagis_active, "</br>",
-                           "<b>Detection Probabilities:</b>", iptds$detection_prob, "</br>",
-                           "<b>Biomark Integrated O&M Site:</b>", iptds$integrated_om_site, "</br>",
-                           "<b>Status and Trends:</b>", iptds$adult_status_trends, "</br>",
-                           "<b>Current Funding:</b>", iptds$current_funding, "</br>",
-                           "<b>BPA Funding Type:</b>", iptds$bpa_funding, "</br>",
-                           "<b>O&M Agency:</b>", iptds$om_agency, "</br>",
-                           "<b>Site Description:</b>", iptds$site_description, "</br>")) %>%
-  # color by integrated O&M contract
-  addCircleMarkers(data = iptds,
-                   group = "Integrated O&M Sites",
-                   color = ~int_om_col(integrated_om_site)) %>%
-  # color by status and trends tier
-  addCircleMarkers(data = iptds,
-                   group = "Status and Trends",
-                   color = ~status_and_trends_col(adult_status_trends)) %>%
-  addCircleMarkers(data = iptds,
-                   group = "O&M Funding Source(s)",
-                   color = ~funding_col(bpa_funding)) %>%
-  addCircles(data = sr_traps_weirs,
-             group = "Traps and Weirs",
-             label = ~paste0(site_code, ": ", name),
-             color = "hotpink",
-             opacity = 1,
-             weight = 5) %>%
-  # control layers
-  addLayersControl(baseGroups = c("Chinook Salmon Populations",
-                                  "Chinook Spawning Areas",
-                                  "Steelhead Populations",
-                                  "Steelhead Spawning Areas"),
-                   overlayGroups = c("All IPTDS Sites",
-                                     "Integrated O&M Sites",
-                                     "Status and Trends",
-                                     "O&M Funding Source(s)",
-                                     "Traps and Weirs"),
+                   overlayGroups = c("IPTDS Recommendations",
+                                     "Current IPTDS Sites",
+                                     "DABOM MRR Sites",
+                                     "Action Priority"),
                    options = layersControlOptions(collapsed = FALSE)) %>%
   # add legends
-  addLegend(data = chnk_spawn,
+  # addLegend(data = sthd_spawn,       # consider moving this to polygon labels, instead
+  #           position = "bottomleft",
+  #           pal = sthd_spawn_col,
+  #           values = ~TYPE,
+  #           title = "Spawning Area Type",
+  #           group = "Steelhead Spawning Areas",
+  #           opacity = 0.5) %>%
+  # addLegend(data = chnk_spawn,       # consider moving this to polygon labels, instead
+  #           position = "bottomleft",
+  #           pal = chnk_spawn_col,
+  #           values = ~TYPE,
+  #           title = "Spawning Area Type",
+  #           group = "Chinook Spawning Areas",
+  #           opacity = 0.5) %>%
+  addLegend(data = iptds_rec,        # consider moving to lower-left, if other legends are removed
             position = "bottomleft",
-            pal = chnk_spawn_col,
-            values = ~TYPE,
-            title = "Spawning Area Type",
-            group = "Chinook Spawning Areas",
-            opacity = 0.5) %>%
-  addLegend(data = sthd_spawn_wgs84,
+            pal = rec_col,
+            values = ~factor(recommendation, levels = c("Continue Funding", 
+                                                        "Proposed New Site", 
+                                                        "Candidate for O&M Project", 
+                                                        "Decommission, Remove, or Transfer")),
+            title = "IPTDS Recommendations",
+            group = "IPTDS Recommendations",
+            opacity = 0.8) %>%
+  addLegend(data = iptds_cur,
             position = "bottomleft",
-            pal = sthd_spawn_col,
-            values = ~TYPE,
-            title = "Spawning Area Type",
-            group = "Steelhead Spawning Areas",
+            pal = cur_col,
+            values = ~factor(integrated_om_site, levels = c("TRUE", "FALSE")),
+            title = "Integrated O&M Site",
+            group = "Current IPTDS Sites",
+            opacity = 0.8) %>%
+  addLegend(data = iptds_rec, 
+            position = "bottomright",
+            pal = priority_col,
+            values = ~factor(action_priority, levels = c("HIGH", "MED", "LOW")),
+            title = "Action Priority",
+            group = "Action Priority",
             opacity = 0.5) %>%
-  addLegend(data = iptds, 
-            position = "bottomleft",
-            pal = int_om_col,
-            values = ~integrated_om_site,
-            title = "Integrated O&M Sites",
-            group = "Integrated O&M Sites",
-            opacity = 0.5) %>%
-  addLegend(data = iptds, 
-            position = "bottomleft",
-            pal = status_and_trends_col,
-            values = ~adult_status_trends,
-            title = "Status and Trends",
-            group = "Status and Trends",
-            opacity = 0.5) %>%
-  addLegend(data = iptds, 
-            position = "bottomleft",
-            pal = funding_col,
-            values = ~bpa_funding,
-            title = "O&M Funding Source(s)",
-            group = "O&M Funding Source(s)",
-            opacity = 0.5) %>%
-  hideGroup("Integrated O&M Sites") %>%
-  hideGroup("Status and Trends") %>%
-  hideGroup("O&M Funding Source(s)") %>%
-  hideGroup("Traps and Weirs") %>%
-  addMiniMap()
+  hideGroup("DABOM MRR Sites") %>%
+  hideGroup("Action Priority")
 
 sr_iptds_leaflet
-
-# save leaflet
-saveWidget(sr_iptds_leaflet, file = here("shiny/leaflet/sr_iptds_leaflet.html"))
 
 ### END SCRIPT
