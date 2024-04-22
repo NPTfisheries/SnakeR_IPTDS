d658389a53a3007ae0b53a306692555ac08e8561
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-04-22 11:59:32 -0600

    started iptds_operations.R

--- R/iptds_operations.R
+++ R/iptds_operations.R
@@ -12,5 +12,41 @@ rm(list = ls())
 
 # load packages
 library(tidyverse)
-library(here)
-library(sf)
+library(PITcleanr)
+library(janitor)
+
+# query interrogation site metadata
+iptds_meta = queryInterrogationMeta() %>%
+  clean_names()
+  
+# snake river interrogation site meta
+sr_iptds_meta = iptds_meta %>%
+  filter(str_starts(rkm, "522"),
+         site_type %in% c("Instream Remote Detection System",
+                          "Adult Fishway")) %>%
+  select(site_code,
+         name,
+         active,
+         operational,
+         first_year,
+         last_year,
+         first_date,
+         last_date,
+         last_file_opened_on,
+         operation_period,
+         operations_organization_code,
+         rkm,
+         site_type,
+         latitude,
+         longitude)
+
+# snake river interrogation site configuration
+sr_iptds_config = queryInterrogationConfig() %>%
+  clean_names() %>%
+  filter(site_code %in% sr_iptds_meta$site_code)
+
+# virtual test tags
+# get code from PITcleanr::queryTimerTagSite() 
+
+### END SCRIPT
+
